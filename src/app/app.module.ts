ef80d6a9e30f97f544ff79e886e593b81260587e
DeadWinnerv <vitwinner@ya.ru>
2023-06-25 14:43:42 +0300

    inital

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { DevExtremeModule } from 'devextreme-angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { DxMenuModule } from "devextreme-angular";
+import { TableComponent } from './components/table/table.component';
+import { HeaderComponent } from './components/header/header.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableComponent,
+    HeaderComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DxMenuModule,
+    DevExtremeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
